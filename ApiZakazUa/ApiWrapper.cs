35d67dbe4b794d43974921de9474d8dfcd09b818
qualterz <qualterz@tutamail.com>
2022-06-15 16:31:28 +0300

    Move high-level methods to extension class

--- ApiZakazUa/ApiWrapper.cs
+++ ApiZakazUa/ApiWrapper.cs
@@ -7,7 +7,6 @@ public class ApiWrapper
 {
     readonly RestClient client;
 
-
     public ApiWrapper()
     {
         client = new RestClient("https://stores-api.zakaz.ua");
@@ -18,69 +17,9 @@ public class ApiWrapper
         return client.GetJson<HashSet<Category>>($"stores/{storeId}/categories");
     }
 
-    public IReadOnlySet<Category>? GetCategories()
-    {
-        var stores = GetStores();
-
-        if (stores == null)
-            return null;
-
-        var result = new HashSet<Category>();
-
-        var storeIds = stores.Select(d => d.Id);
-        foreach (var id in storeIds) 
-        {
-            var categories = GetCategories(id);
-            if (categories != null)
-                result.UnionWith(categories);
-        }
-
-        if (result.Any()) return result; else return null;
-    }
-
     public IReadOnlySet<Product>? GetProducts(int storeId, string categoryId)
     {
         return client.GetJson<Products>($"stores/{storeId}/categories/{categoryId}/products")?.Results.ToHashSet();
-    }
-
-    public IReadOnlySet<Product>? GetProducts(int storeId)
-    {
-        var categories = GetCategories(storeId);
-        if (categories == null)
-            return null;
-
-        var result = new HashSet<Product>();
-
-        var categoryIds = categories.Select(d => d.Id);
-        foreach (var id in categoryIds)
-        {
-            var products = GetProducts(storeId, id);
-
-            if (products != null)
-                result.UnionWith(products);
-        }
-
-        if (result.Any()) return result; else return null;
-    }
-
-    public IReadOnlySet<Product>? GetProducts()
-    {
-        var stores = GetStores();
-        if (stores == null)
-            return null;
-
-        var result = new HashSet<Product>();
-
-        var storeIds = stores.Select(d => d.Id);
-        foreach (var id in storeIds)
-        {
-            var products = GetProducts(id);
-
-            if (products != null)
-                result.UnionWith(products);
-        }
-
-        if (result.Any()) return result; else return null;
     }
 
     public IReadOnlySet<Store>? GetStores()
---
+++ ApiZakazUa/Extensions.cs
@@ -0,0 +1,66 @@
+using ApiZakazUa.Records;
+
+namespace ApiZakazUa.Extensions;
+
+public static class WrapperExtensions
+{
+    public static IReadOnlySet<Category>? GetCategories(this ApiWrapper wrapper)
+    {
+        var stores = wrapper.GetStores();
+
+        if (stores == null)
+            return null;
+
+        var result = new HashSet<Category>();
+
+        var storeIds = stores.Select(d => d.Id);
+        foreach (var id in storeIds) 
+        {
+            var categories = wrapper.GetCategories(id);
+            if (categories != null)
+                result.UnionWith(categories);
+        }
+
+        if (result.Any()) return result; else return null;
+    }
+
+    public static IReadOnlySet<Product>? GetProducts(this ApiWrapper wrapper, int storeId)
+    {
+        var categories = wrapper.GetCategories(storeId);
+        if (categories == null)
+            return null;
+
+        var result = new HashSet<Product>();
+
+        var categoryIds = categories.Select(d => d.Id);
+        foreach (var id in categoryIds)
+        {
+            var products = wrapper.GetProducts(storeId, id);
+
+            if (products != null)
+                result.UnionWith(products);
+        }
+
+        if (result.Any()) return result; else return null;
+    }
+
+    public static IReadOnlySet<Product>? GetProducts(this ApiWrapper wrapper)
+    {
+        var stores = wrapper.GetStores();
+        if (stores == null)
+            return null;
+
+        var result = new HashSet<Product>();
+
+        var storeIds = stores.Select(d => d.Id);
+        foreach (var id in storeIds)
+        {
+            var products = wrapper.GetProducts(id);
+
+            if (products != null)
+                result.UnionWith(products);
+        }
+
+        if (result.Any()) return result; else return null;
+    }
+}
