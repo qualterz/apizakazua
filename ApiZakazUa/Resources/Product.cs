fd92f1a51fc4aed118fbe96556a4b5b6d83f0981
qualterz <qualterz@tutamail.com>
2023-07-05 13:39:42 +0300

    Add additional fields for product record

--- ApiZakazUa/Resources/Product.cs
+++ ApiZakazUa/Resources/Product.cs
@@ -9,17 +9,45 @@ public record Discount(
     [property: JsonPropertyName("due_date")] object DueDate
 );
 
-public record Producer(
-    [property: JsonPropertyName("trademark")] string Trademark,
-    [property: JsonPropertyName("trademark_slug")] string TrademarkSlug,
-    [property: JsonPropertyName("website")] string Website,
-    [property: JsonPropertyName("name")] string Name
+public record Gallery(
+    [property: JsonPropertyName("s150x150")] string S150x150,
+    [property: JsonPropertyName("s200x200")] string S200x200,
+    [property: JsonPropertyName("s350x350")] string S350x350,
+    [property: JsonPropertyName("s1350x1350")] string S1350x1350
+);
+
+public record Image(
+    [property: JsonPropertyName("s150x150")] string S150x150,
+    [property: JsonPropertyName("s200x200")] string S200x200,
+    [property: JsonPropertyName("s350x350")] string S350x350,
+    [property: JsonPropertyName("s1350x1350")] string S1350x1350
+);
+
+public record Logo(
+    [property: JsonPropertyName("s16x16")] string S16x16,
+    [property: JsonPropertyName("s32x32")] string S32x32,
+    [property: JsonPropertyName("s64x64")] string S64x64
+);
+
+public record NutritionFacts(
+    [property: JsonPropertyName("ingredient_energy")] string IngredientEnergy,
+    [property: JsonPropertyName("ingredient_protein")] string IngredientProtein,
+    [property: JsonPropertyName("ingredient_fat")] string IngredientFat,
+    [property: JsonPropertyName("ingredient_carbohydrates")] string IngredientCarbohydrates
 );
 
 public record Quantity(
     [property: JsonPropertyName("min")] int Min,
     [property: JsonPropertyName("step")] int Step,
     [property: JsonPropertyName("is_strict")] bool IsStrict
+);
+
+public record Producer(
+    [property: JsonPropertyName("trademark")] string Trademark,
+    [property: JsonPropertyName("trademark_slug")] string TrademarkSlug,
+    [property: JsonPropertyName("website")] string Website,
+    [property: JsonPropertyName("logo")] Logo Logo,
+    [property: JsonPropertyName("name")] string Name
 );
 
 public record Restrictions(
@@ -36,12 +64,13 @@ public record Product(
     [property: JsonPropertyName("discount")] Discount Discount,
     [property: JsonPropertyName("bundle")] int Bundle,
     [property: JsonPropertyName("unit")] string Unit,
-    [property: JsonPropertyName("volume")] object Volume,
+    [property: JsonPropertyName("volume")] double? Volume,
     [property: JsonPropertyName("quantity")] Quantity Quantity,
     [property: JsonPropertyName("currency")] string Currency,
     [property: JsonPropertyName("category_id")] string CategoryId,
     [property: JsonPropertyName("parent_category_id")] string ParentCategoryId,
     [property: JsonPropertyName("description")] string Description,
+    [property: JsonPropertyName("nutrition_facts")] NutritionFacts NutritionFacts,
     [property: JsonPropertyName("slug")] string Slug,
     [property: JsonPropertyName("in_stock")] bool InStock,
     [property: JsonPropertyName("is_hit")] bool IsHit,
@@ -53,13 +82,16 @@ public record Product(
     [property: JsonPropertyName("excisable")] bool Excisable,
     [property: JsonPropertyName("web_url")] string WebUrl,
     [property: JsonPropertyName("restrictions")] Restrictions Restrictions,
-    [property: JsonPropertyName("ingredients")] IReadOnlyList<object> Ingredients,
+    [property: JsonPropertyName("ingredients")] IReadOnlyList<string> Ingredients,
     [property: JsonPropertyName("fat")] object Fat,
     [property: JsonPropertyName("shelf_life")] string ShelfLife,
     [property: JsonPropertyName("temperature_range")] string TemperatureRange,
     [property: JsonPropertyName("pack_amount")] object PackAmount,
     [property: JsonPropertyName("country")] string Country,
     [property: JsonPropertyName("producer")] Producer Producer,
+    [property: JsonPropertyName("custom_ribbons")] IReadOnlyList<object> CustomRibbons,
+    [property: JsonPropertyName("img")] Image Image,
+    [property: JsonPropertyName("gallery")] IReadOnlyList<Gallery> Gallery,
     [property: JsonPropertyName("is_uber_item")] bool IsUberItem,
     [property: JsonPropertyName("taxons")] IReadOnlyList<object> Taxons,
     [property: JsonPropertyName("weight")] double Weight,
